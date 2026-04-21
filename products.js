be890e5c2c220560dc4f09b7345530380224aeaa
snehalbhosale417-web <snehalbhosale417@gmail.com>
2026-04-21 14:06:21 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,36 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  image: String,
-  category: String
-});
+const Product = require("./Product");
 
-const Product = mongoose.model("Product", productSchema);
-
-
+// Get all products
 router.get("/", async (req, res) => {
   const products = await Product.find();
   res.json(products);
 });
 
-
+// Add product
 router.post("/", async (req, res) => {
   const product = new Product(req.body);
   await product.save();
   res.json(product);
 });
 
-
+// Delete product
 router.delete("/:id", async (req, res) => {
   await Product.findByIdAndDelete(req.params.id);
   res.json({ message: "Deleted" });
 });
 
-
+// Update product
 router.put("/:id", async (req, res) => {
   await Product.findByIdAndUpdate(req.params.id, req.body);
   res.json({ message: "Updated" });
